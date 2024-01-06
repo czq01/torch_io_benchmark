4f1b4979898bb076b5180d3d4fc00471b642adc0
czq01 <czq0106@outlook.com>
2024-01-05 21:44:07 -0800

    Feat&Fix: tensorclass & Dataset class
    1. add tensorclass Dataset definition
    2. add tensorclass timecheck impl
    3. add Dataset class to keep up format with tensorclass

--- torch_test.py
+++ torch_test.py
@@ -1,10 +1,41 @@
 from typing import Callable, Tuple
-from torch.utils.data import DataLoader, TensorDataset
+from torch.utils.data import DataLoader, Dataset
+from tensordict import tensorclass
 import numpy as np
 import torch
 import time
 
-BATCH_SIZE = 1
+BATCH_SIZE = 5
+
+class NumericDataSet(Dataset):
+    def __init__(self, features, target):
+        self.len = len(features)
+        self.features = torch.from_numpy(features).cuda()
+        self.target = torch.from_numpy(target).cuda()
+
+    def __getitem__(self, index):
+        return self.features[index], self.target[index]
+
+    def __len__(self):
+        return self.len
+
+
+@tensorclass
+class NumericTensorClass(Dataset):
+    features: torch.Tensor
+    target: torch.Tensor
+
+    @classmethod
+    def from_data(cls, features, target, batch_size):
+        data = cls(
+            features = torch.from_numpy(features).cuda(),
+            target = torch.from_numpy(target).cuda(),
+            batch_size = [len(features)]
+        )
+        # data.memmap_()
+        return data
+
+
 
 def get_time_cost(func: Callable):
     def _wrapper(*args, **kwargs):
@@ -19,47 +50,55 @@ def get_time_cost(func: Callable):
 # 共同外部数据
 def get_test_data(r: int, c: int) -> Tuple[np.ndarray]:
     return (np.random.random((r, c)),  # X (c features)
-            np.random.random((r, 1)))  # Y
-
-
-# 训练函数
-def train_func(data: DataLoader[Tuple[torch.Tensor,...]], cuda = False):
-    c = data.dataset.tensors[0].shape[1]
-    resid = torch.zeros(size=(c, 1))
-    if (cuda): resid = resid.cuda()
-    for epoch in range(10**1):
-        i=0
-        for x, y in data:
-            if (cuda):
-                # TODO if any better way ??? (one-time copy?)
-                x = x.cuda()
-                y = y.cuda()
-            # any custom func
-            resid += y-x.mean()
-            i += 1
-    return resid.cpu()
+            np.random.random((r, 1)),  # Y
+            np.random.randint(0, r))   #labels
 
 
 @get_time_cost
-def dataloader_test(data: Tuple[np.ndarray], cuda=False):
-    x = torch.from_numpy(data[0])
-    y = torch.from_numpy(data[1])
-    train = TensorDataset(x, y)
-    train_loader = DataLoader(dataset=train, batch_size=BATCH_SIZE,
-                              shuffle=True, num_workers=1)
-    val = train_func(train_loader, cuda)
-    return val
+def dataloader_test(data: Tuple[np.ndarray], cuda=True, load_only=True):
+    train = NumericDataSet(data[0], data[1])
+    train_loader = DataLoader(dataset=train, batch_size=BATCH_SIZE,shuffle=True)
+    if (load_only): return
+    resid = torch.zeros(size=(BATCH_SIZE, 1))
+    if (cuda): resid = resid.cuda()
+    for epoch in range(10**1):
+        i=0
+        for x, y in train_loader:
+            # any custom func
+            resid += y-x.mean(1).unsqueeze(1)
+            i += 1
+    return resid.mean().cpu()
+
+
+@get_time_cost
+def tensorclass_test(data: Tuple[np.array], cuda=True, load_only=True):
+    train = NumericTensorClass.from_data(features = data[0],
+                         target = data[1], batch_size=[BATCH_SIZE])
+    train_loader = DataLoader(train, batch_size=BATCH_SIZE, shuffle=True,
+                                collate_fn=lambda x: x)
+    if (load_only): return True
+    resid = torch.zeros(size=(BATCH_SIZE, 1))
+    if (cuda): resid = resid.cuda()
+    for epoch in range(10**1):
+        i=0
+        for batch in train_loader:
+            x = batch.features
+            y = batch.target
+            # any custom func
+            resid += y-x.mean(1).unsqueeze(1)
+            i += 1
+    return resid.mean().cpu()
 
 
 def main():
     # TODO  for data_size in (100, 50), (1000, 500) ...
-    data = get_test_data(1000, 500)
-    result = dataloader_test(data, True)
-    # TODO result = tensorDict_test()
-
+    size = (10000, 5000)
+    data = get_test_data(size[0], size[1])
+    result = dataloader_test(data, True, load_only=False)
+    result = tensorclass_test(data, True, load_only=False)
     return
 
-# training_data = DataLoader()
+
 if __name__ == "__main__":
     torch.cuda.set_device(0)
     main()
